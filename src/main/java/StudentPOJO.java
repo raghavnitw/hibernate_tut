f52677093a82ed65aa14d777d83c634079f374f4
raghav_fk <raghavender.m@flipkart.com>
2016-08-04 18:03:17 +0530

    hb_tut

---
+++ src/main/java/StudentPOJO.java
@@ -0,0 +1,30 @@
+public class StudentPOJO{
+
+private int stNo;
+private String stName;
+private String stAddress;
+
+public void setStNo(int stNo){
+  this.stNo = stNo;
+}
+
+public int getStNo(){
+  return this.stNo;
+}
+
+public void setStName(String name){
+  this.stName = name;
+}
+
+public String getStName(){
+  return this.stName;
+}
+
+public void setStAddress(String addr){
+  this.stAddress = addr;
+}
+public String getStAddress(){
+  return this.stAddress;
+}
+
+}
